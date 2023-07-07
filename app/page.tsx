2ea9529c6a919edf9c69914b04cbd86d642b5d68
0xpetra <franco.petraz@gmail.com>
2023-07-07 13:41:45 +0200

    👌 IMPROVE: remove logo animatioln

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ const navigation = [
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+			<div className="center">
+				<Logo />
+			</div>
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex items-center justify-center gap-4">
 					{/* {navigation.map((item) => (
@@ -25,13 +28,11 @@ export default function Home() {
 				</ul>
 			</nav>
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+
 			<Particles
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={100}
 			/>
-			<div className="center">
-				<Logo />
-			</div>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
 				Geo Stamp Network
 			</h1>
