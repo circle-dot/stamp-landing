87dbe6afe7b0b3c634013cd000f7b3ebcea41ac3
0xpetra <franco.petraz@gmail.com>
2023-07-21 16:47:33 +0200

    📦 NEW:

--- app/page.tsx
+++ app/page.tsx
@@ -11,27 +11,14 @@ const navigation = [
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tr from-[#1A2467] via-zinc-600/20 to-[#1A2467]">
-			<div className="center">
+			<div className="my-20 center">
 				<Logo />
 			</div>
-			<nav className="my-16 animate-fade-in">
-				<ul className="flex items-center justify-center gap-4">
-					{/* {navigation.map((item) => (
-						<Link
-							key={item.href}
-							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-						>
-							{item.name}
-						</Link>
-					))} */}
-				</ul>
-			</nav>
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
 			<Particles
 				className="absolute inset-0 -z-10 animate-fade-in"
-				quantity={100}
+				quantity={300}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-[#FDE884] cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
 				Stamp Network
